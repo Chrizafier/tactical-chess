391a6f78c0bc0a8738b34ea9be51fbc761d1f8bf
HusnainChoudhry <143745002+HusnainChoudhry@users.noreply.github.com>
2024-06-25 12:59:41 -0400

    Made taskbar dynamic across different devices

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,12 +4,20 @@ import { useRouter, Slot } from "expo-router";
 
 import * as React from 'react';
 import { Appbar, Avatar } from 'react-native-paper';
+import { useWindowDimensions, View } from 'react-native';
+import { Dimensions} from 'react-native'; // Add this line
+
 
 export default function AppLayout() {
     const router = useRouter();
+    const screenWidth = useWindowDimensions().width;
+    const appBarWidth = screenWidth; // Adjust this value as needed
+
+
 
     return (
         <>
+            <View style={{ width: appBarWidth, alignSelf: 'center' }}>
             <Appbar.Header>
                 <Appbar.BackAction onPress={ () => router.back() } />
                 <Appbar.Content title='Tactical Chess' />
@@ -19,6 +27,7 @@ export default function AppLayout() {
                 <Appbar.Action icon='account-cog' onPress={() => router.push('/user')} />
                 <Appbar.Action icon='login' onPress={ () => router.push('/login') } />
             </Appbar.Header>
+            </View>
             <Slot />
         </>
     );
